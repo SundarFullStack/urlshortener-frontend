5521c0d6ef47a899d574fe05511ca0215c56e8af
Sundar Thambidurai <126937777+SundarFullStack@users.noreply.github.com>
2023-08-10 14:28:21 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -22,7 +22,7 @@ const Dashboard = () => {
   const DashboardValid = async () => {
     let token = localStorage.getItem("usersdatatoken");
 
-    const res = await fetch("/validuser", {
+    const res = await fetch("https://urlshortner-backend-jwc7.onrender.com/validuser", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
